02431a093f4af108fe584428eaf762b95ef91fb9
Edgars Putka <edgars.putka>
2018-07-09 14:01:16 +0300

    webhooks to discord channel

--- reporter.rb
+++ reporter.rb
@@ -1,13 +1,17 @@
 require_relative 'features/support/api_helper'
 require 'json'
 
+job_name = ARGV[0]
+build_number = ARGV[1]
+build_url = ARGV[2]
+
 fields = []
 
 thumbnail = {'url': 'https://pbs.twimg.com/profile_images/662215297378574336/YOOD0ijr_400x400.png'}
 fields.push({'name' => 'Autors:', 'value' => 'Edgars Putka'})
-fields.push({'name' => 'Job:', 'value' => 'job_name'})
-fields.push({'name' => 'Build number:', 'value' => 'build_number'})
-fields.push({'name' => 'Build URL:', 'value' => 'build_url'})
+fields.push({'name' => 'Job:', 'value' => job_name})
+fields.push({'name' => 'Build number:', 'value' => build_number})
+fields.push({'name' => 'Build URL:', 'value' => build_url})
 
 
 embed = []
